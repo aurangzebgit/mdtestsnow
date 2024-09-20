63078110561ac6ee5de29c08990b5c0e0f3da55b
aurangzebgit <mdaurangzeb245@gmail.com>
2024-09-20 12:28:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.write('Hello world!')
 
+st.write('Welcome to the world of Streamlit!')
+st.write('This is where you can create beautiful web applications with ease.')
+st.write('Let’s explore the capabilities of this powerful library together!')
+
 
 
 
